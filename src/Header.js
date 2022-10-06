a2851a20088d46a82d404e19560e0c41bf6b7dc9
Pornsawan Kantirangsima <pornsawan_kan@truecorp.co.th>
2022-10-06 14:54:34 +0700

    test no ff1

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,19 @@
 export default function Header() {
   return (
-    <div className='flex justify-between px-14 text-white font-bold p-8'>
-      <div className='text-3xl'>Remix</div>
-      <nav className='text-neutral-400 text-lg'>
-        <a className='px-4' href='https://remix.run/blog'>
+    <div className="flex justify-between px-14 text-white font-bold p-8">
+      <div className="text-3xl">Remix</div>
+      <nav className="text-neutral-400 text-lg">
+        <a className="px-4" href="https://remix.run/blog">
           Blog
         </a>
-        <a className='px-4' href='https://remix.run/docs/en/v1'>
+        <a className="px-4" href="https://remix.run/docs/en/v1">
           Docs
         </a>
-        <a className='px-4' href='https://github.com/remix-run'>
+        <a className="px-4" href="https://github.com/remix-run">
           Github
         </a>
-        <a className='px-4' href='https://remix.run/conf/2023'>
-          Remix Conf
+        <a className="px-4" href="https://remix.run/conf/2023">
+          Remix Confxxxx
         </a>
       </nav>
     </div>
